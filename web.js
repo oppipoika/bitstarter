c03d9eb132bb27a70de3158196007fce241c23cd
Janne Pirkkanen <jp@oppipoika.net>
2013-07-11 16:43:24 +0000

    Changes to web.js

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require("fs");
+
+var fileName = "index.html";
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send('Hello World2-v2!');
 });
 
 var port = process.env.PORT || 5000;
